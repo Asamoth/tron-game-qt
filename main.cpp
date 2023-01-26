2e79a030b6823b21f8664a9629f6d53dfdf279a8
Asamoth <117095706+Asamoth@users.noreply.github.com>
2023-01-26 20:16:39 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,16 +4,14 @@
 
 Menu *menu;
 /*
- * Ce jeu de Tron consiste en deux joueurs qui se déplacent en laissant derrière eux une trainée.
- * Les joueurs ne peuvent pas traverser les murs ni les trainées des autres joueurs.
- * Le but du jeu est de faire sortir l'adversaire de la grille en lui coupant la route ou en le faisant entrer en collision avec sa propre trainée.
- * Les joueurs peuvent se déplacer dans les quatre directions (haut, bas, gauche, droite) en utilisant les touches de leur clavier.
- * Les deux joueurs jouent sur le même clavier - le premier joueur avec les touches WASD, et le second joueur avec les touches flêchées du clavier.
- * Le joueur qui gagne est celui qui parvient à survivre le plus longtemps.
- * Le jeu est crée pour être joué en difficulté difficile - les autres difficultés créent certains problèmes mineur d'affichage lorsque les joueurs tournent
- * mais aucun réel problème de comportement.
- *
- * Je n'ai pas de fichier .ui à joindre car j'ai codé manuellement mon jeu au lieu d'utiliser des templates.
+* This game of Tron consists of two players who move leaving behind a trail.
+* Players cannot pass through other walls or any players' trails, including their own.
+* The object of the game is to get the opponent out of the grid by cutting them off and leading them colliding with any trail, or the walls.
+* Players can move in all four directions (up, down, left, right) using their keyboard keys.
+* Both players play on the same keyboard - the first player with the WASD keys, and the second player with the arrow keys on the keyboard.
+* The winning player is the one who manages to survive the longest.
+* The game is designed to be played on hard difficulty - other difficulties create some minor display issues when players turn
+* but no real behavior problems.
 */
 int main(int argc, char *argv[])
 {
